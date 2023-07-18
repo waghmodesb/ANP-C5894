bd092b752325a76ae2e539b74f3be4e7730912d5
waghmodesb <130727958+waghmodesb@users.noreply.github.com>
2023-07-18 12:34:39 +0530

    Add files via upload

---
+++ Hands-off/FibonaccisSeries.java
@@ -0,0 +1,41 @@
+package loop;
+import java.util.Scanner;
+
+public class FibonaccisSeries {
+
+	public static void main(String[] args) {
+		//input from the user
+		  Scanner input = new Scanner(System.in);
+
+	        System.out.print("Enter the limit: ");
+	        int limit = input.nextInt();
+	     // First number in the Fibonacci series
+	        int num1 = 0; 
+	     // Second number in the Fibonacci series
+	        int num2 = 1; 
+	     // Variable to store the current Fibonacci number
+	        int fibonacci = 0;
+
+	        System.out.print("Fibonacci series up to " + limit + ": ");
+
+	        while (fibonacci <= limit) {
+	            if (fibonacci % 3 == 0) {
+	                // Skip numbers divisible by 3
+	                fibonacci = num1 + num2; // Calculate the next Fibonacci number
+	                num1 = num2; // Update the first number
+	                num2 = fibonacci; // Update the second number
+	                continue; // Skip printing the current Fibonacci number
+	            }
+
+	            System.out.print(fibonacci + " "); // Print the current Fibonacci number
+
+	            fibonacci = num1 + num2; // Calculate the next Fibonacci number
+	            num1 = num2; // Update the first number
+	            num2 = fibonacci; // Update the second number
+	        }
+
+	        input.close();
+	    }
+	}
+
+	
